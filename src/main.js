fa71106c590abdf5ca0462b1cac4395783343a48
ZjVirus <285857527@qq.com>
2017-12-15 18:05:21 +0800

    添加图表

--- src/main.js
+++ src/main.js
@@ -11,6 +11,20 @@ import 'font-awesome/css/font-awesome.css'
 import './assets/css/index.scss'
 import './assets/css/scrollBar.scss'
 // import ChangeTheme from './components/changeTheme/changeTheme'
+// 图表
+import VeLine from 'v-charts/lib/line'  // 折线
+import VeBar from 'v-charts/lib/bar'     // 条形图
+import VePie from 'v-charts/lib/pie'     // 饼图
+import VeRing from 'v-charts/lib/ring'   // 环形图
+import VeTitle from 'echarts/lib/component/title'
+Vue.component(VeLine.name, VeLine)
+Vue.component(VePie.name, VePie)
+Vue.component(VeBar.name, VeBar)
+Vue.component(VeRing.name, VeRing)
+Vue.use( VeTitle)
+// echarts
+import echarts from 'echarts'
+Vue.use(echarts)
 // vuex 
 import store from './store/index'
 import axios from 'axios'
@@ -26,12 +40,12 @@ axios.defaults.baseURL = state.httpUrl
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 router.beforeEach((to, from, next) => {
-  console.log(to)
+  // console.log(to)
   if(to.path == '/Login') {
     sessionStorage.removeItem('user')
   }
   let user = JSON.stringify(sessionStorage.getItem('user'))
-  console.log(user == 'null')
+  // console.log(user == 'null')
   if(user == 'null' && to.path !== '/Login') {
     next({ path: "/Login" })
   } else {
